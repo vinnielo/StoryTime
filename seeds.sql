83c0c643ec6609cab2dac80cb483d4f12e50ac7f
jose Lopez <vinnielo01@gmail.com>
2020-06-04 09:04:40 -0700

    updated and cleaned up seeds

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,18 @@
 -- KIDS SEEDS
-INSERT INTO kids (name, pet, createdAt, updatedAt) VALUES ('Holden', 'Ziggy', "00:00:00", "00:00:00" );
-INSERT INTO kids (name,  createdAt, updatedAt) VALUES ('Hunter',  "00:00:00", "00:00:00" );
-INSERT INTO kids (name, pet, createdAt, updatedAt, sibling) VALUES ('Tommy', 'Misty', "00:00:00", "00:00:00", "Ben" );
+INSERT INTO kids (name, pet, createdAt, updatedAt) VALUES ('Holden', 'Ziggy', '00:00:00', '00:00:00' );
+INSERT INTO kids (name,  createdAt, updatedAt) VALUES ('Hunter',  '00:00:00', '00:00:00' );
+INSERT INTO kids (name, pet, createdAt, updatedAt, sibling) VALUES ('Tommy', 'Misty', '00:00:00', '00:00:00', 'Ben' );
 
 -- GUARDIAN SEEDS
 
-INSERT INTO guardians (name) VALUES (Cyrus);
-INSERT INTO guardians (name) VALUES (Erik);
+INSERT INTO guardians (name, createdAt, updatedAt) VALUES ('Cyrus', '00:00:00', '00:00:00');
+INSERT INTO guardians (name, createdAt, updatedAt) VALUES ('Erik', '00:00:00', '00:00:00');
 
 -- USER SEEDS
 
-INSERT INTO users (email, password) VALUES (test@test.com, abc123);
-INSERT INTO users (email, password) VALUES (test1@test.com, 123abc);
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('test@test.com', 'abc123', '00:00:00', '00:00:00');
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('test1@test.com', '123abc', '00:00:00', '00:00:00');
+
+select * from users;
+select * from kids;
+select * from guardians;
